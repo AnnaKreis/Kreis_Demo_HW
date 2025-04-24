fe3829fd168caf4a081e1581b26826754dd2bd6a
anyat <anyatarara93@gmail.com>
2025-04-24 09:56:28 +0200

    HW25(24)

--- src/test/java/com/demo/tests/TestBase.java
+++ src/test/java/com/demo/tests/TestBase.java
@@ -2,12 +2,15 @@ package com.demo.tests;
 
 import com.demo.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser",
             Browser.CHROME.browserName()));
@@ -28,6 +31,15 @@ public class TestBase {
     @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+    @BeforeMethod
+    public void startTest(){
+        logger.info("Start test");
+    }
+
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
     }
 
 }
